e8cfea4f5b6580d6627e80049a4395494c8c2fb8
surya <surya.ramshere@gmail.com>
2016-11-01 15:29:03 +0700

    Create Factorial Console Program

--- Program.cs
+++ Program.cs
@@ -4,12 +4,21 @@ using System.Xml.Linq;
 using System.Collections.Generic;
 using Exercise.Repository;
 using Exercise.Service;
+using System.Numerics;
 
 namespace Exercise {
     public class Program {
         static void Main(string[] args)
         {
-            Console.WriteLine("Done");
+            int numberInt = int.Parse(Console.ReadLine());
+            BigInteger result = numberInt;
+
+            for (int i = 1; i < numberInt; i++)
+            {
+                result = result * i;
+            }
+
+            Console.WriteLine(result);
         }
 
     }
